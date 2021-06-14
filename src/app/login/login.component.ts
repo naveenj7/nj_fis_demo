3bf116aff75e399f96f706221649c3c87fd91201
Ambarishsi <Viratsinghxyz444@gmail.com>
2021-06-14 12:07:06 +0530

    initial checkin
    initial checkin

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  //templateUrl: './login.component.html',
+  template: `<p>hi how are you</p>
+                <signup></signup>`,
+  styleUrls: ['./login.component.scss']
+
+})
+
+export class LoginComponent {
+
+}
