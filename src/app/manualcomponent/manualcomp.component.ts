ac9ccce29b08866e99b8d22f53d0c2269c409978
Ambarishsi <Viratsinghxyz444@gmail.com>
2021-06-17 10:58:07 +0530

    adding directive

--- src/app/manualcomponent/manualcomp.component.ts
+++ src/app/manualcomponent/manualcomp.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 
 export class ManualcompComponent {
 
+  userName1:string = "ambarish";
+  age:number = 25;
+  twoWayBinding:string = "test of two way binding"
+  btnClick: boolean = false;
+  vals = ['aa','bb','cc','dd','ee'];
+
+  somevariable:boolean = true;
+
+  twoway:string = "sometext";
+  anyMethod(){
+    return "microservices";
+  }
+
+  clickEventMethod(e: any){
+
+    console.log(e);
+    alert("hello alert");
+  }
+
+
 }
+
+
--- src/app/teamcard/teamcard.component.ts
+++ src/app/teamcard/teamcard.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TeamcardComponent implements OnInit {
-
+  btnClicks: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
