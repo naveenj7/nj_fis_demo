c4fa4b9557292c42a03e98f827a9be865a86363d
Ambarishsi <Viratsinghxyz444@gmail.com>
2021-06-18 11:50:27 +0530

    directive code

--- src/app/manualcomponent/manualcomp.component.ts
+++ src/app/manualcomponent/manualcomp.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fisdemo-manulacomp',
-    templateUrl: './manualcomp.component.html'
+    templateUrl: './manualcomp.component.html',
+    styleUrls: ['./manualcomp.component.scss']
   })
 
 export class ManualcompComponent {
@@ -10,12 +11,17 @@ export class ManualcompComponent {
   userName1:string = "ambarish";
   age:number = 25;
   twoWayBinding:string = "test of two way binding"
-  btnClick: boolean = false;
-  vals = ['aa','bb','cc','dd','ee'];
+  btnClick: boolean = true;
+  vals = ['aa','bb','cc','dd','ee','ff','gg','hhhhhhh'];
+
+  ngClassCondition: boolean = false;
+
+  showIt:boolean = false;
+
 
   somevariable:boolean = true;
 
-  twoway:string = "sometext";
+  twoway:string = "initial text";
   anyMethod(){
     return "microservices";
   }
